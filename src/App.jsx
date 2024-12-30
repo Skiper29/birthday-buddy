68eb0170015e42c11fe7de88262a18023645c6cc
Skiper29 <61919416+Skiper29@users.noreply.github.com>
2024-12-30 15:00:48 +0200

    add birthday reminder feature with person list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import { useState } from "react";
+
+import persons from "./data";
+import List from "./components/List";
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(persons);
+
+  return (
+    <main>
+      <section className="container">
+        <h2>{people.length} Birthdays today</h2>
+        <List people={people} />
+      </section>
+    </main>
+  );
 };
 export default App;
